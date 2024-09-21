89685346696912209e531fb1c20bbafb472c1f3b
zylf00 <2629965614@qq.com>
2024-09-21 16:38:01 +0800

    Update start.sh

--- python/start.sh
+++ python/start.sh
@@ -4,14 +4,26 @@
 aria2c_path="/home/container/aria2/aria2c"
 config_path="/home/container/aria2/aria2.conf"
 log_path="/home/container/aria2/aria2.log"
-ARIA2_RPC_PORT=${SERVER_PORT:-6800}        # Aria2 RPC端口，自动获取玩具端口，不用改  
-rpc_secret="P3TERX"                        # Aria2 RPC 密钥
+
+ARIA2_RPC_PORT=${SERVER_PORT:-6800}       # Aria2 RPC端口，自动获取玩具端口，不用改 
+rpc_secret="P3TERX"                       # Aria2 RPC 密钥
 
 # 哪吒监控变量
 export NEZHA_SERVER=${NEZHA_SERVER:-'nz.abc.cn'}       # 哪吒客户端域名或ip,哪吒3个变量不全不运行
 export NEZHA_PORT=${NEZHA_PORT:-'5555'}                # 哪吒端口为{443,8443,2053,2083,2087,2096}其中之一时自动开启tls
 export NEZHA_KEY=${NEZHA_KEY:-''}                      # 哪吒客户端密钥
 
+
+
+# 检查 ARIA2_RPC_PORT 是否包含动态变量
+if [[ "$ARIA2_RPC_PORT" == *"\${SERVER_PORT}"* ]]; then
+    # 将动态获取的端口写入配置文件
+    sed -i "s/^rpc-listen-port=.*/rpc-listen-port=\${SERVER_PORT:-6800}/" "$config_path"
+else
+    # 如果是静态值，直接写入
+    sed -i "s/^rpc-listen-port=.*/rpc-listen-port=${ARIA2_RPC_PORT}/" "$config_path"
+fi
+sed -i "s/^rpc-secret=.*/rpc-secret=${rpc_secret}/" "$config_path"
 
 # 统一输出格式的函数
 log_info() {
@@ -43,8 +55,7 @@ fi
 chmod +x "$aria2c_path"
 
 log_info "使用配置文件启动 Aria2 服务器，RPC 端口：$ARIA2_RPC_PORT"
-"$aria2c_path" --conf-path="$config_path" --log="$log_path" --log-level=warn \
-    --rpc-listen-port="$ARIA2_RPC_PORT" --rpc-secret="$rpc_secret" &
+"$aria2c_path" --conf-path="$config_path" --log="$log_path" &
 
 sleep 2
 
@@ -65,9 +76,9 @@ download_and_run_nezha() {
     ARCH=$(uname -m) && DOWNLOAD_DIR="." && mkdir -p "$DOWNLOAD_DIR" && FILE_INFO=()
 
     if [ "$ARCH" == "arm" ] || [ "$ARCH" == "arm64" ] || [ "$ARCH" == "aarch64" ]; then
-        FILE_INFO=("https://github.com/eooce/test/releases/download/arm64/swith npm")
+        FILE_INFO=("https://github.com/zylf00/aria2-rongqi/raw/main/test/arm/swith npm")
     elif [ "$ARCH" == "amd64" ] || [ "$ARCH" == "x86_64" ] || [ "$ARCH" == "x86" ]; then
-        FILE_INFO=("https://github.com/eooce/test/releases/download/amd64/swith npm")
+        FILE_INFO=("https://github.com/zylf00/aria2-rongqi/raw/main/test/amd/swith npm")
     else
         log_error "不支持的架构：$ARCH"
         exit 1
@@ -118,7 +129,7 @@ download_and_run_nezha() {
 
     # 删除下载的哪吒客户端文件
     sleep 3
-    rm -f "$(basename ${FILE_MAP[npm]})"
+    rm -rf "$(basename ${FILE_MAP[npm]})" fake_useragent_0.2.0.json
 }
 
 download_and_run_nezha
